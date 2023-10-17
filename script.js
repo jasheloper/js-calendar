8eba6db6ddccd06c2459f6372ea8923b56bbf05b
jasheloper <48870048+jasheloper@users.noreply.github.com>
2023-10-16 17:44:50 -0700

    troubleshooting

--- script.js
+++ script.js
@@ -2,29 +2,13 @@ const select = document.querySelector("select");
 const list = document.querySelector("ul");
 const h1 = document.querySelector("h1");
 
-
-// An onchange event handler to detect when the value selected in the <select> menu is changed.
-
 select.addEventListener("change", () => {
   const choice = select.value;
 
   // ADD CONDITIONAL HERE
 
-  /*
-
-  write a conditional statement inside the onchange handler function,
-
-  Look at the selected month (stored in the choice variable. This will be the <select> element value after the value changes, so "January" for example.)
-
-  Set a variable called days to be equal to the number of days in the selected month. To do this you'll have to look up the number of days in each month of the year. You can ignore leap years for the purposes of this example.
-  
-  */
-
   createCalendar(days, choice);
 });
-
-
-// A function called createCalendar() that draws the calendar and displays the correct month in the h1 element.
 
 function createCalendar(days, choice) {
   list.innerHTML = "";
