8cdf56f3723c1cada7b3214c75dcf1234fe65c8f
jasheloper <48870048+jasheloper@users.noreply.github.com>
2023-10-16 17:46:42 -0700

    reset

--- script.js
+++ script.js
@@ -7,8 +7,19 @@ select.addEventListener("change", () => {
 
   // ADD CONDITIONAL HERE
 
+    /*
+    write a conditional statement inside the onchange handler function
+
+    Look at the selected month (stored in the choice variable. This will be the <select> element value after the value changes, so "January" for example.)
+
+    Set a variable called days to be equal to the number of days in the selected month. To do this you'll have to look up the number of days in each month of the year. You can ignore leap years for the purposes of this example.
+
+    */
+
   createCalendar(days, choice);
 });
+
+// A function called createCalendar() that draws the calendar and displays the correct month in the h1 element.
 
 function createCalendar(days, choice) {
   list.innerHTML = "";
